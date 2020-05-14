5eb1405de67313a7be5c94640a2d06bb912c92b2
Matthias Loibl <mail@matthiasloibl.com>
2020-05-14 23:22:06 +0200

    Update main.go to reflect statuspage effort

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/ghodss/yaml"
@@ -18,28 +19,56 @@ import (
 )
 
 type configuration struct {
-	PrometheusURL string `json:"prometheus_url"`
-	Projects      []struct {
-		Name          string `json:"name"`
-		Days          uint   `json:"days"`
-		PrometheusURL string `json:"prometheus_url"`
-		Data          []struct {
-			Title string `json:"title"`
-			SLO   struct {
-				Value      float64 `json:"value"`
-				Comparison string  `json:"comparison"`
-			} `json:"slo"`
-			Format string `json:"format"`
-			Query  string `json:"query"`
-		} `json:"data"`
-	} `json:"projects"`
+	PrometheusURL string        `json:"prometheus_url"`
+	Applications  []Application `json:"applications"`
+	//Projects      []struct {
+	//	Name          string `json:"name"`
+	//	Days          uint   `json:"days"`
+	//	PrometheusURL string `json:"prometheus_url"`
+	//	Objective     struct {
+	//		Target float64 `json:"target"`
+	//		Query  string  `json:"query"`
+	//	} `json:"objective"`
+	//	Data []struct {
+	//		Title string `json:"title"`
+	//		SLO   struct {
+	//			Value      float64 `json:"value"`
+	//			Comparison string  `json:"comparison"`
+	//		} `json:"slo"`
+	//		Format string `json:"format"`
+	//		Query  string `json:"query"`
+	//	} `json:"data"`
+	//} `json:"projects"`
+}
+
+type Application struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Components  []struct {
+		Name   string `json:"name"`
+		Status struct {
+			Degraded struct {
+				Query string `json:"query"`
+			} `json:"degraded"`
+			Outage struct {
+				Query string `json:"query"`
+			} `json:"outage"`
+		} `json:"status"`
+	} `json:"components"`
 }
 
 type Project struct {
 	Name          string
 	PrometheusURL string
+	Objective     Objective
 	Dates         []time.Time
 	Columns       []Column
+}
+
+type Objective struct {
+	Value  float64
+	Target float64
+	Days   uint
 }
 
 type Column struct {
@@ -118,22 +147,22 @@ func action(c *cli.Context) error {
 		return fmt.Errorf("failed to create Prometheus client: %w", err)
 	}
 
-	promAPI := prometheusv1.NewAPI(client)
+	_ = prometheusv1.NewAPI(client)
 
-	for _, p := range config.Projects {
-		for _, d := range p.Data {
-			value, _, err := promAPI.Query(context.TODO(), d.Query, time.Now())
-			if err != nil {
-				return fmt.Errorf("failed to run query: %w", err)
-			}
-
-			vec := value.(model.Vector)
-
-			for _, v := range vec {
-				fmt.Printf("%.2f\n", v.Value)
-			}
-		}
-	}
+	//for _, p := range config.Projects {
+	//	for _, d := range p.Data {
+	//		value, _, err := promAPI.Query(context.TODO(), d.Query, time.Now())
+	//		if err != nil {
+	//			return fmt.Errorf("failed to run query: %w", err)
+	//		}
+	//
+	//		vec := value.(model.Vector)
+	//
+	//		for _, v := range vec {
+	//			fmt.Printf("%.2f\n", v.Value)
+	//		}
+	//	}
+	//}
 
 	return nil
 }
@@ -149,81 +178,161 @@ func volatile(c *cli.Context) error {
 		return fmt.Errorf("failed to parse config: %w", err)
 	}
 
-	projects, err := query(config)
+	_, err = query(config)
 	if err != nil {
 		return fmt.Errorf("failed querying: %w", err)
 	}
 
-	return serve(projects)
+	return nil
+
+	//return serve(projects)
 }
 
-func query(config configuration) ([]Project, error) {
-	var projects []Project
+func query(config configuration) ([]Application, error) {
+	var applications []Application
 
-	for _, cp := range config.Projects {
-		var prometheusURL string
-		if cp.PrometheusURL != "" {
-			prometheusURL = cp.PrometheusURL // use project's Prometheus URL
-		} else if config.PrometheusURL != "" {
-			prometheusURL = config.PrometheusURL // use global Prometheus URL
-		} else {
-			return nil, fmt.Errorf("no Prometheus URL found")
-		}
-
-		client, err := api.NewClient(api.Config{Address: prometheusURL})
+	for _, application := range config.Applications {
+		client, err := api.NewClient(api.Config{Address: config.PrometheusURL})
 		if err != nil {
 			return nil, fmt.Errorf("failed to create Prometheus client: %w", err)
 		}
 		promAPI := prometheusv1.NewAPI(client)
 
-		project := Project{Name: cp.Name}
-
-		// This is basically midnight just one second less to still have the date of the previous day
-		year, month, day := time.Now().Date()
-		midnight := time.Date(year, month, day, 0, 0, 0, 0, time.UTC).Add(-1 * time.Second)
-
-		for i := 0; i < int(cp.Days); i++ {
-			project.Dates = append(project.Dates, midnight.AddDate(0, 0, -i))
-		}
-
-		for _, d := range cp.Data {
-			c := Column{Title: d.Title, SLO: SLO{
-				Value:      d.SLO.Value,
-				Comparison: d.SLO.Comparison,
-			}}
-
-			fmt.Println(c.Title)
-
-			switch d.Format {
-			case string(seconds):
-				c.Format = seconds
-			case string(percentage):
-				c.Format = percentage
-			case string(percentage100):
-				c.Format = percentage100
+		for _, component := range application.Components {
+			value, _, err := promAPI.Query(context.TODO(), component.Status.Degraded.Query, time.Now())
+			if err != nil {
+				return nil, nil
 			}
 
-			for i := 0; i < int(cp.Days); i++ {
-				day := midnight.AddDate(0, 0, -i)
-				fmt.Printf("\t%s\n", day.Format("2006-01-02"))
-
-				value, _, err := promAPI.Query(context.TODO(), d.Query, day)
-				if err != nil {
-					return nil, fmt.Errorf("querying failed: %w", err)
-				}
-
-				var result float64
-				vec := value.(model.Vector)
-				for _, v := range vec {
-					result = float64(v.Value)
-				}
-				c.Data = append(c.Data, result)
+			var result float64
+			vec := value.(model.Vector)
+			for _, v := range vec {
+				result = float64(v.Value)
 			}
-			project.Columns = append(project.Columns, c)
+
+			fmt.Printf("%s - %s - degraded: %v\n", application.Name, component.Name, result == 1.0)
+
+			value, _, err = promAPI.Query(context.TODO(), component.Status.Outage.Query, time.Now())
+			if err != nil {
+				return nil, fmt.Errorf("failed to query Prometheus: %v", err)
+			}
+
+			vec = value.(model.Vector)
+			for _, v := range vec {
+				result = float64(v.Value)
+			}
+
+			fmt.Printf("%s - %s - outage: %v\n", application.Name, component.Name, result == 1.0)
 		}
-		projects = append(projects, project)
+
 	}
-	return projects, nil
+
+	return applications, nil
+
+	//for _, cp := range config.Projects {
+	//	var prometheusURL string
+	//	if cp.PrometheusURL != "" {
+	//		prometheusURL = cp.PrometheusURL // use project's Prometheus URL
+	//	} else if config.PrometheusURL != "" {
+	//		prometheusURL = config.PrometheusURL // use global Prometheus URL
+	//	} else {
+	//		return nil, fmt.Errorf("no Prometheus URL found")
+	//	}
+	//
+	//	client, err := api.NewClient(api.Config{Address: prometheusURL})
+	//	if err != nil {
+	//		return nil, fmt.Errorf("failed to create Prometheus client: %w", err)
+	//	}
+	//	promAPI := prometheusv1.NewAPI(client)
+	//
+	//	project := Project{Name: cp.Name}
+	//
+	//	objective, err := queryObjective(promAPI, cp.Objective.Query, cp.Days)
+	//	if err != nil {
+	//		return nil, err
+	//	}
+	//
+	//	project.Objective = Objective{
+	//		Value:  objective,
+	//		Target: cp.Objective.Target,
+	//		Days:   cp.Days,
+	//	}
+	//
+	//	fmt.Println("budget", time.Duration((1-project.Objective.Target)*3600*24*float64(project.Objective.Days))*time.Second)
+	//	fmt.Println("budget left", time.Duration((project.Objective.Value-project.Objective.Target)*3600*24*float64(project.Objective.Days))*time.Second)
+	//
+	//	projects = append(projects, project)
+	//
+	//	continue
+	//
+	//	// This is basically midnight just one second less to still have the date of the previous day
+	//	year, month, day := time.Now().Date()
+	//	midnight := time.Date(year, month, day, 0, 0, 0, 0, time.UTC).Add(-1 * time.Second)
+	//
+	//	for i := 0; i < int(cp.Days); i++ {
+	//		project.Dates = append(project.Dates, midnight.AddDate(0, 0, -i))
+	//	}
+	//
+	//	for _, d := range cp.Data {
+	//		c := Column{Title: d.Title, SLO: SLO{
+	//			Value:      d.SLO.Value,
+	//			Comparison: d.SLO.Comparison,
+	//		}}
+	//
+	//		fmt.Println(c.Title)
+	//
+	//		switch d.Format {
+	//		case string(seconds):
+	//			c.Format = seconds
+	//		case string(percentage):
+	//			c.Format = percentage
+	//		case string(percentage100):
+	//			c.Format = percentage100
+	//		}
+	//
+	//		for i := 0; i < int(cp.Days); i++ {
+	//			day := midnight.AddDate(0, 0, -i)
+	//			fmt.Printf("\t%s\n", day.Format("2006-01-02"))
+	//
+	//			query := strings.Replace(d.Query, "$interval", "24h", -1)
+	//
+	//			value, _, err := promAPI.Query(context.TODO(), query, day)
+	//			if err != nil {
+	//				return nil, fmt.Errorf("querying failed: %w", err)
+	//			}
+	//
+	//			var result float64
+	//			vec := value.(model.Vector)
+	//			for _, v := range vec {
+	//				result = float64(v.Value)
+	//			}
+	//			c.Data = append(c.Data, result)
+	//		}
+	//		project.Columns = append(project.Columns, c)
+	//	}
+	//	projects = append(projects, project)
+	//}
+	//return , nil
+}
+
+func queryObjective(api prometheusv1.API, query string, days uint) (float64, error) {
+	q := prepareQuery(query, fmt.Sprintf("%dd", days))
+
+	value, _, err := api.Query(context.TODO(), q, time.Now())
+	if err != nil {
+		return 0, err
+	}
+
+	var result float64
+	vec := value.(model.Vector)
+	for _, v := range vec {
+		result = float64(v.Value)
+	}
+	return result, nil
+}
+
+func prepareQuery(q string, interval string) string {
+	return strings.Replace(q, "$interval", interval, -1)
 }
 
 func serve(projects []Project) error {
